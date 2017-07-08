313db8d01c59b3172b34b3a9fc17da1c9c0b8ace
Saxon Knight <knight7@hawaii.edu>
2017-07-07 16:25:08 -1000

    Comment setup_ubuntu1604.sh

--- setup_ubuntu1604.sh
+++ setup_ubuntu1604.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-sudo apt install python-pip
+# get pip
+sudo apt install python-pip -y
+# set up python virtual environment
 pip install virtualenv
 virtualenv .
 source bin/activate
+# install python dependencies
 pip install tensorflow
 pip install pillow
 pip install lxml
 pip install jupyter
 pip install matplotlib
 pip install ipython
+# get git
+sudo apt install git -y
+# clone tensorflow/models repository
+git clone https://github.com/tensorflow/models.git
 cd models
-sudo apt-get install protobuf-compiler
+# get protobuf compiler
+sudo apt install protobuf-compiler -y
+# compile the protobuf libraries and add to python path
 protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/slim
+# run tests
 python object_detection/builders/model_builder_test.py
+# run object_detection_tutorial
 jupyter notebook object_detection/object_detection_tutorial.ipynb
