e16ab285bd60a8f361f9ef65761d1eeff507c504
Saxon Knight <knight7@hawaii.edu>
2017-08-02 14:10:57 -1000

    Add TODOs to default_analysis.py

---
+++ default_analysis.py
@@ -0,0 +1,3 @@
+# Read EXIF and ODAPI output from images table.
+# Identify different objects by some relevant common factor.
+# Consolidate information about individual objects into JSON files in objects table.
