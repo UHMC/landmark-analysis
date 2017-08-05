8094c9d05af051ce6331e471d1a2016ef4f4e3d9
Saxon Knight <knight7@hawaii.edu>
2017-08-04 15:54:29 -1000

    Add ObjectDB configuration to default_analysis.py

--- default_analysis.py
+++ default_analysis.py
@@ -1,3 +1,12 @@
 # Read EXIF and ODAPI output from images table.
 # Identify different objects by some relevant common factor.
 # Consolidate information about individual objects into JSON files in objects table.
+
+# ObjectDB Configuration
+config = {
+    'user': 'machine',
+    'password': 'learning',
+    'host': '127.0.0.1',
+    'database': 'ObjectDB',
+    'raise_on_warnings': True,
+}
