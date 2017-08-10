455d2766320bc116a4fbf4d249fd9a9cab56b128
Saxon Knight <knight7@hawaii.edu>
2017-08-10 10:42:56 -1000

    Refactor analysis routines into nested loops
    - Also swaps object_array with object_dict
    - Adds SUFFICIENT_SCORE_THRESHOLD (unnamed prior) with default value 0.5
    - Does away with extraneous string formatting previously introduced
    - Slight changes to comments to match changes
    - Score threshold check changed from '>' to '>='
    
    The previous state of the routines for this operation were hacked
    together to facilitate testing and review, but they were not ultimately
    up to snuff and needed to be revamped.
    
    The change from object_array to object_dict is to facilitate extensible
    analysis approaches without necessitating modification of this code and
    ultimately enable easy storage and retrieval of scores, filenames, etc.

--- default_analysis.py
+++ default_analysis.py
@@ -8,6 +8,8 @@ import mysql.connector
 from mysql.connector import errorcode
 import jsonpickle
 
+SUFFICIENT_SCORE_THRESHOLD=0.5
+
 # ObjectDB Configuration
 config = {
     'user': 'machine',
@@ -45,38 +47,30 @@ cursor.execute('SELECT odapi_output FROM images')
 for row in cursor:
     odapi_output_filenames.append(str(row[0]))
 
-# Extract all data into an array (does not scale well, but we're pressed for time).
-odapi_data_array=[]
+# Extract all objects with sufficient scores into an array.
 os.chdir('/srv/ObjectDB/odapi_output')
+object_dict={}
 for filename in odapi_output_filenames:
     odapi_file=open(filename,'rb')
     odapi_data=jsonpickle.decode(odapi_file.read())
     odapi_file.close()
-    odapi_data_array.append(odapi_data)
-# Loop through odapi_data_array and populate object_array
-object_array=[]
-# For reference, odapi_adapter.get_objects(filepath) returns: [
-#   num_detections,
-#   boxes,
-#   scores,
-#   classes,
-#   image_np,
-#   categories,
-#   category_index
-# ]
-for odapi_data in odapi_data_array:
-    # image_np: uint8 numpy array with shape (img_height, img_width, 3)
-    # boxes: a numpy array of shape [N, 4]
-    # classes: a numpy array of shape [N]
-    # scores: a numpy array of shape [N] or None.
-    # category_index: a dict containing category dictionaries (each holding
-    #   category index `id` and category name `name`) keyed by category indices.
+    # Populate object_dict.
+    # odapi_data is a tuple containing:
+    #   image_np: uint8 numpy array with shape (img_height, img_width, 3)
+    #   boxes: a numpy array of shape [N, 4]
+    #   classes: a numpy array of shape [N]
+    #   scores: a numpy array of shape [N] or None.
+    #   category_index: a dict containing category dictionaries (each holding
+    #       category index `id` and category name `name`) keyed by category indices.
     for i in range(odapi_data[1].shape[0]):
         score=odapi_data[3][i]
-        if score>.5:
-            name_text = 'name: {}'.format(odapi_data[4][odapi_data[2][i]]['name'])
-            score_text='score: {}%'.format(int(100*score))
-            object_array.append((name_text,score_text))
+        if score>=SUFFICIENT_SCORE_THRESHOLD:
+            name=odapi_data[4][odapi_data[2][i]]['name']
+            if object_dict.has_key(name):
+                object_dict[name][1].append(score)
+                object_dict[name][2].append(filename)
+            else:
+                object_dict[name]=[name, [score], [filename]]
 
 # Print output
-print(object_array)
+print(object_dict)
