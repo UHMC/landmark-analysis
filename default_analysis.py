6710098a21951bc638b6273ae2bdeb2077b31132
Mario Canul <mcanul@hawaii.edu>
2017-08-11 10:31:50 -1000

    Add logic using db info to daemonize analysis

--- default_analysis.py
+++ default_analysis.py
@@ -7,6 +7,7 @@ import os
 import mysql.connector
 from mysql.connector import errorcode
 import pickle
+import time
 
 SUFFICIENT_SCORE_THRESHOLD=0.5
 
@@ -47,45 +48,66 @@ for row in cursor:
 # Extract all objects with sufficient scores into an array.
 os.chdir('/srv/ObjectDB/odapi_output')
 object_dict={}
-for filename in odapi_output_filenames:
-    odapi_file=open(filename,'rb')
-    odapi_data=pickle.load(odapi_file)
-    odapi_file.close()
-    # Populate object_dict.
-    # odapi_data is a tuple containing:
-    #   image_np: uint8 numpy array with shape (img_height, img_width, 3)
-    #   boxes: a numpy array of shape [N, 4]
-    #   classes: a numpy array of shape [N]
-    #   scores: a numpy array of shape [N] or None.
-    #   category_index: a dict containing category dictionaries (each holding
-    #       category index `id` and category name `name`) keyed by category indices.
-    for i in range(odapi_data[1].shape[0]):
-        score=odapi_data[3][i]
-        if score>=SUFFICIENT_SCORE_THRESHOLD:
-            name=odapi_data[4][odapi_data[2][i]]['name']
-            if name in object_dict:
-                object_dict[name][1].append(score)
-                object_dict[name][2].append(filename)
-            else:
-                object_dict[name]=[name, [score], [filename]]
 
-# Actual analysis
-for name in object_dict:
-    obj=object_dict[name]
-    # For each object:
-    #   - number of images this kind appears in across database
-    num_images=len(set(obj[2]))
-    #   - total number of times it appears in the database
-    num_appearances=len(obj[1])
-    #   - simple mean of percent match scores across database
-    mean_score=sum(obj[1])/float(len(obj[1]))
-    # Put these in the database and file system ^
-    analysis_output={
-        name:obj,
-        "num_images":num_images,
-        "num_appearances":num_appearances,
-        "mean_score":mean_score
-    }
+# Variable setup
+last_entry = 0
+known_entry = 0
 
-# Print output
-print(object_dict)
+# Find number of the last entry in the database
+cursor.execute("SELECT id FROM images ORDER BY id DESC LIMIT 1")
+last_entry = int(cursor.fetchone()[0])
+
+
+while(True):
+    # DEBUGGING
+    print("DEBUG: last_entry = %s | known_entry = %s", last_entry, known_entry)
+
+    # Run the for loops if the known entry does not match the last entry  (database updated)
+    if (last_entry != known_entry):
+        for filename in odapi_output_filenames:
+            odapi_file=open(filename,'rb')
+            odapi_data=pickle.load(odapi_file)
+            odapi_file.close()
+            # Populate object_dict.
+            # odapi_data is a tuple containing:
+            #   image_np: uint8 numpy array with shape (img_height, img_width, 3)
+            #   boxes: a numpy array of shape [N, 4]
+            #   classes: a numpy array of shape [N]
+            #   scores: a numpy array of shape [N] or None.
+            #   category_index: a dict containing category dictionaries (each holding
+            #       category index `id` and category name `name`) keyed by category indices.
+            for i in range(odapi_data[1].shape[0]):
+                score=odapi_data[3][i]
+                if score>=SUFFICIENT_SCORE_THRESHOLD:
+                    name=odapi_data[4][odapi_data[2][i]]['name']
+                    if name in object_dict:
+                        object_dict[name][1].append(score)
+                        object_dict[name][2].append(filename)
+                    else:
+                        object_dict[name]=[name, [score], [filename]]
+
+            # Actual analysis
+        for name in object_dict:
+            obj=object_dict[name]
+            # For each object:
+            #   - number of images this kind appears in across database
+            num_images=len(set(obj[2]))
+            #   - total number of times it appears in the database
+            num_appearances=len(obj[1])
+            #   - simple mean of percent match scores across database
+            mean_score=sum(obj[1])/float(len(obj[1]))
+            # Put these in the database and file system ^
+            analysis_output={
+                name:obj,
+                "num_images":num_images,
+                "num_appearances":num_appearances,
+                "mean_score":mean_score
+            }
+            # Print output
+        print(object_dict)
+        # Become aware of the last entry in the database
+        known_entry = last_entry
+        # Refresh the last entry from database
+        cursor.execute("SELECT id FROM images ORDER BY id DESC LIMIT 1")
+        last_entry = int(cursor.fetchone()[0])
+        time.sleep(3)
