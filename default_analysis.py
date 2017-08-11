954be739dc6f81585ab88d0731a93ecaa3282fe1
Saxon Knight <knight7@hawaii.edu>
2017-08-10 16:04:29 -1000

    Implement simple analysis

--- default_analysis.py
+++ default_analysis.py
@@ -69,5 +69,18 @@ for filename in odapi_output_filenames:
             else:
                 object_dict[name]=[name, [score], [filename]]
 
+# Actual analysis
+for name in object_dict:
+    obj=object_dict[name]
+    # For each object:
+    #   - number of images this kind appears in across database
+    num_images=len(set(obj[2]))
+    #   - total number of times it appears in the database
+    num_appearances=len(obj[1])
+    #   - simple mean of percent match scores across database
+    mean_score=sum(obj[1])/float(len(obj[1]))
+    # Put these in the database and file system ^
+    pass
+
 # Print output
 print(object_dict)
