f8f23e632c0abe70b41d6ba38810f6c14f737e63
Saxon Knight <knight7@hawaii.edu>
2017-08-11 17:32:34 -1000

    Reformat code in image_strip.py

--- image_strip.py
+++ image_strip.py
@@ -1,12 +1,13 @@
+import os
+import pickle
+import time
+import uuid
+
+import exifread
 import mysql.connector
 from mysql.connector import errorcode
-import exifread
-import os
-import time
-import pickle
-import uuid
-import odapi_adapter
 
+import odapi_adapter
 
 config = {
     'user': 'machine',
@@ -19,7 +20,6 @@ config = {
 add_image = ("INSERT INTO images "
              "(path,exif,odapi_output) "
              "VALUES (%s, %s, %s)")
-
 
 # Connecting to the Database using config
 try:
@@ -34,28 +34,30 @@ except mysql.connector.Error as err:
 
 # Changing working directory to unprocessed folder
 os.chdir('/srv/ObjectDB/unprocessed')
-cursor = db.cursor() # Create a cursor for MySQL commands
+cursor = db.cursor()  # Create a cursor for MySQL commands
 
 # Loop forever
-while(True):
-	files = os.listdir('.') # Populate list of files
-	if(files != ''): # Only run when folder is not empty
-		for x in files:
-			print('Processing file: '+x) 
-			f = open(x,'rb') # Open the file 
-			filename = uuid.uuid4().hex
-			odapi_output = pickle.dumps(odapi_adapter.get_objects(f.name)) # Extract odapi_output into JSON
-			exif = pickle.dumps(exifread.process_file(f)) # Extract EXIF into JSON
-			os.system('mv '+ x+ ' ../processed/'+filename) # Move file to processed folder
-			os.chdir('/srv/ObjectDB/EXIF')
-			with open(filename+"-exif.json", 'wb') as output: # Dump EXIF into /srv/ObjectDB/EXIF
-				output.write(exif)
-			os.chdir('/srv/ObjectDB/odapi_output')
-			with open(filename+"-odapi_output.json", 'wb') as output: # Dump ODAPI output into /srv/ObjectDB/odapi_output
-				output.write(odapi_output)
-			os.chdir('/srv/ObjectDB/unprocessed')
-			data_image = ('/srv/ObjectDB/processed/'+filename,'/srv/ObjectDB/EXIF/'+filename+'-exif.json','/srv/ObjectDB/odapi_output/'+filename+'-odapi_output.json') # Information to be added to the database
-			cursor.execute( add_image, data_image) # Add item to the database
-			db.commit() # Commit changes to database
-	time.sleep(1)
-	print('Waiting for files...')
+while (True):
+    files = os.listdir('.')  # Populate list of files
+    if (files != ''):  # Only run when folder is not empty
+        for x in files:
+            print('Processing file: ' + x)
+            f = open(x, 'rb')  # Open the file
+            filename = uuid.uuid4().hex
+            odapi_output = pickle.dumps(odapi_adapter.get_objects(f.name))  # Extract odapi_output into JSON
+            exif = pickle.dumps(exifread.process_file(f))  # Extract EXIF into JSON
+            os.system('mv ' + x + ' ../processed/' + filename)  # Move file to processed folder
+            os.chdir('/srv/ObjectDB/EXIF')
+            with open(filename + "-exif.json", 'wb') as output:  # Dump EXIF into /srv/ObjectDB/EXIF
+                output.write(exif)
+            os.chdir('/srv/ObjectDB/odapi_output')
+            with open(filename + "-odapi_output.json",
+                      'wb') as output:  # Dump ODAPI output into /srv/ObjectDB/odapi_output
+                output.write(odapi_output)
+            os.chdir('/srv/ObjectDB/unprocessed')
+            data_image = ('/srv/ObjectDB/processed/' + filename, '/srv/ObjectDB/EXIF/' + filename + '-exif.json',
+                          '/srv/ObjectDB/odapi_output/' + filename + '-odapi_output.json')  # Information to be added to the database
+            cursor.execute(add_image, data_image)  # Add item to the database
+            db.commit()  # Commit changes to database
+    time.sleep(1)
+    print('Waiting for files...')
