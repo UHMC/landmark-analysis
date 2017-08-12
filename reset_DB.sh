0ed6eb5795e17d7bfcf673911bace21cd8a89bb4
Saxon Knight <knight7@hawaii.edu>
2017-08-12 11:53:32 -1000

    Include removal of analysis files in reset_DB.sh

--- reset_DB.sh
+++ reset_DB.sh
@@ -3,6 +3,7 @@ rm -rf /srv/ObjectDB/processed/*
 rm -rf /srv/ObjectDB/unprocessed/*
 rm -rf /srv/ObjectDB/EXIF/*
 rm -rf /srv/ObjectDB/odapi_output/*
+rm -rf /srv/ObjectDB/analysis/*
 mysql -u machine -plearning << EOF
 USE ObjectDB
 TRUNCATE images;
